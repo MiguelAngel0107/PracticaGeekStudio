629bf2279214df99bcc9e70e7e8c9f7621212f01
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2022-09-28 20:36:56 -0500

    PurasFunciones

--- GeekStudio/Ce.js
+++ GeekStudio/Ce.js
@@ -1,4 +1,33 @@
 import {Datos} from "./Datos.js";
 
-let Valor1= Datos.agricola.ValorCuantitativo.I1[0];
-console.log(Valor1);
+let Valor1= Datos.agricola.ValorCuantitativo.I1;
+
+//El paramentro arr, tiene que ser un indicador.
+const EncontrarValor=(arr, input, sector)=>{
+    let valor=0;
+    for(let i=0; i<arr.length; i++)
+    {
+        let Arr=arr[i][0];
+        for(let j=0;j<Arr.length;j++)
+        {
+            let Arreglo=arr[i][0][j];
+            //console.log(Arreglo);
+            if(Arreglo==input)
+            {
+                //console.log("Si esta en el arreglo")
+                switch(i)
+                {
+                    case 0: valor=15; return valor;
+                    case 1: valor=36; return valor;
+                    case 2: valor=83; return valor;
+                    case 3: valor=103; return valor;
+                    case 4: valor=70; return valor;
+                }
+                
+            }
+        }
+    }
+} 
+console.log(EncontrarValor(Valor1, 61))
+
+    
--- GeekStudio/Datos.js
+++ GeekStudio/Datos.js
@@ -1,37 +1,38 @@
+//Esta funcion sirve para crear rangos con numeros enteros
 function range(start, stop=undefined, step=1) {
     const startArray = stop  === undefined ? 0 : start;
     const stopArray = stop  === undefined ? start : stop;
     return Array.from({ length: (stopArray - startArray) / step + 1}, (_, i) => startArray + (i * step));
 }
+//Esta funcion recibe con primer parametro, un arreglo de numeros enteros, y un limite decimal (opcional), a su vez te devolvera un arreglo 
+//con numeros decimales
 function rangeD( rango, limite ){
     const Arreglo1 = rango;
     const Arreglo2=[];
     Arreglo1.forEach(function(elemento, indice, array) {
         for(let i=0; i<10; i++)
         {
-            if(!(Arreglo1.length==elemento))
-            {
                 let aux= i/10;
                 let result;
                 result = elemento +aux;
                 Arreglo2.push(result);
-                
                 if(result==limite){return Arreglo2;}
-            }
-
         }
     })
     return Arreglo2;
 }
 
+
 export const Datos = {
     agricola:{
         ValorCuantitativo:{
             I1:[
-                [rangeD(range(1,9))],
-                [range(1,9)],
-                [],
-                []
+                [rangeD(range(1,60)), 15],
+                [rangeD(range(61,74)), 36],
+                [rangeD(range(75,94)), 83],
+                [rangeD(range(95,100)), 103],
+                [["ND", "Sin Informacion"],70]
+
             ],
             I2:[
                 [],
