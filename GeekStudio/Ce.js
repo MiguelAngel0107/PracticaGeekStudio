4e0b2c2aab48e9a4a2091c08f3a2729c79785b62
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2022-09-28 20:42:24 -0500

    Update Ce.js

--- GeekStudio/Ce.js
+++ GeekStudio/Ce.js
@@ -3,7 +3,7 @@ import {Datos} from "./Datos.js";
 let Valor1= Datos.agricola.ValorCuantitativo.I1;
 
 //El paramentro arr, tiene que ser un indicador.
-const EncontrarValor=(arr, input, sector)=>{
+const EncontrarValor=(arr, input)=>{
     let valor=0;
     for(let i=0; i<arr.length; i++)
     {
@@ -14,20 +14,13 @@ const EncontrarValor=(arr, input, sector)=>{
             //console.log(Arreglo);
             if(Arreglo==input)
             {
-                //console.log("Si esta en el arreglo")
-                switch(i)
-                {
-                    case 0: valor=15; return valor;
-                    case 1: valor=36; return valor;
-                    case 2: valor=83; return valor;
-                    case 3: valor=103; return valor;
-                    case 4: valor=70; return valor;
-                }
-                
+                return valor=arr[i][1];
             }
         }
     }
 } 
-console.log(EncontrarValor(Valor1, 61))
+
+
+console.log(EncontrarValor(Valor1, 60))
 
     
